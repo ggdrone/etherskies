5e39407d0986728ac9949b01bf15066f0ae88ce8
timackevald@gmail.com <timackevald@gmail.com>
2025-09-18 12:11:29 +0200

    Removed

--- robin/build/libs/Cities.d
+++
@@ -1,6 +0,0 @@
-build/libs/Cities.o: src/libs/Cities.c src/libs/Cities.h src/libs/Meteo.h \
- src/libs/utils.h src/libs/HTTP.h
-src/libs/Cities.h:
-src/libs/Meteo.h:
-src/libs/utils.h:
-src/libs/HTTP.h:
